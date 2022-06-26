4dd176a74ebd82a983ba7804b298808e8fed1d7b
ChakshuSwami <108181011+ChakshuSwami@users.noreply.github.com>
2022-06-26 20:14:55 +0530

    my my site

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Engineer","Gamer", "Human", "Typist", "Biker","Student :)"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
